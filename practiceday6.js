8d22c05af4974c8d5517de01ac06b32528029e0a
Lewis, Mekhi D <lewismed@iu.edu>
2022-07-12 17:55:57 -0400

    hplz work

---
+++ practiceday6.js
@@ -0,0 +1,43 @@
+const baseURL="some url"\
+const todoPath='todos'
+
+
+
+
+const getTdo=id=>
+
+
+// reject part of proimse argument is rebuked by try catch
+// also the reject part of promises is opional
+
+// the await operator is used to wait for a promise . it can only be used inside of a async fucntion
+
+// awai will also make the reques get sen 1 by 1. Because of this it will cost you a lot of time.
+
+// its used when one request relies on data from a previous request.
+
+
+// resolve() method returns a promise object that is resolved wih a given value.
+
+
+// foreach()- this is basically a built in for loop,. has hree part, (current instance, inialazion, ). for each calls a funcion for each item inside a array
+
+
+//static is needed becasue he deosnt want to implement reject. using static is important becasue he doesnt want to use a catch.
+
+
+//promise has a higher priory than setTimeOut()
+
+
+// event loop
+
+
+// when using static you do no need to use let , const and var to declare things.
+
+
+//overflow makes i so you can scroll up or down and overflow doesn show
+
+//overflow auto in css adds in scroll bar
+
+
+//check what async does
